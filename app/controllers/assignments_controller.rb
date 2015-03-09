36c904b27222e3a444c84dd10d92b98844d5a667
danweinstock <dan.weinstock@yahoo.com>
2015-03-09 18:03:18 -0400

    added assignments manager controller to allow for single responsibility among forms

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,2 +1,10 @@
 class AssignmentsController < ApplicationController
+	def index
+		@assignments = Assignment.all
+	end
+
+	def show
+		  @assignment = Assignment.find(params[:id])
+	end
+
 end
---
+++ app/helpers/assignments_manager_helper.rb
@@ -0,0 +1,2 @@
+module AssignmentsManagerHelper
+end
