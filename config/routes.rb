a419abe5b7639b055609402b462d036cd87b16fa
Christoph Szczechowicz <christoph.szczechowicz@gmail.com>
2015-03-10 05:29:21 -0400

    Sesssions view was created as well as a starting point for the routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
 
   get 'sessions/destroy'
 
+  resources :users, only: [:new, :create]
+    
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'login' => 'sessions#new', as: :login
+  get 'logout' => 'sessions#destroy', as: :logout
+  get 'signup' => 'users#new', :as => :signup   
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
