da1d98daddb9f6cf352d9964aa268fdf627a895d
Christoph Szczechowicz <christoph.szczechowicz@gmail.com>
2015-03-11 16:12:13 -0400

    Changes with assignments controllers

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,10 +1,44 @@
 class AssignmentsController < ApplicationController
+	def new
+		@assignment = Assignment.new
+	end
+
+	def create
+		@made_tasks = Assignment.new(assignment_params)
+		@made_tasks.user_id = current_user.id
+
+		if @assignment.save
+			redirect_to assignment_path(@made_task.id)
+		else 
+			render :new
+		end
+	end
+
+
+	# def show
+	# 	  @assignment_manager = Assignment_manager.find(params[:id])
+	# end
+
 	def index
 		@assignments = Assignment.all
 	end
 
 	def show
 		@assignment = Assignment.find(params[:id])
+	end
+
+	def edit
+		@assignment = Assignment.find(params[:id])
+	end
+
+	private
+		def assignment_params
+		params.require(:assignment).permit(:title,
+			:description,
+			:price_in_cents, 
+			:date,
+			:time
+		)
 	end
 
 end
